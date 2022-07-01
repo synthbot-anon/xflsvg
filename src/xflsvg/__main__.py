dbfd0971070cb78c32d51860e331652f2347cf59
Synthbot Anon <synthbot.anon@gmail.com>
2022-06-30 22:03:35 -0700

    support batch processing

--- src/xflsvg/__main__.py
+++ src/xflsvg/__main__.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import logging
 import os
+import re
 import traceback
 
 from .rendertrace import RenderTracer, RenderTraceReader
@@ -15,14 +16,82 @@ def as_number(data):
     return int.from_bytes(bytes, byteorder="big")
 
 
-def should_process(data, id, par):
-    return as_number(data) % par == id
+def should_process(data, args):
+    return (as_number(data) - args.id) % args.par == 0
 
 
-if __name__ == "__main__":
+def convert(input_path, output_path, args):
+    print("converting", input_path, "->", output_path, end="... ")
+    input_path = os.path.normpath(input_path)
+    if input_path.lower().endswith(".xfl"):
+        input_folder = os.path.dirname(input_path)
+        print("reading from", input_folder)
+        reader = XflReader(input_folder)
+    elif os.path.isdir(input_path):
+        reader = RenderTraceReader(input_path)
+    else:
+        raise Exception(
+            "The input needs to be either an xfl file (/path/to/file.xfl) or a render trace (/path/to/trace/)."
+        )
+
+    output_path = os.path.normpath(output_path)
+    if output_path.lower().endswith(".svg"):
+        renderer = SvgRenderer()
+        parent_dir = os.path.dirname(output_path)
+        if parent_dir:
+            os.makedirs(parent_dir, exist_ok=True)
+        logging.basicConfig(
+            filename=os.path.join(parent_dir, "logs.txt"),
+            level=logging.DEBUG,
+            force=True,
+        )
+    elif os.path.isdir(output_path) or not os.path.exists(output_path):
+        renderer = RenderTracer()
+        os.makedirs(output_path, exist_ok=True)
+        logging.basicConfig(
+            filename=os.path.join(output_path, "logs.txt"),
+            level=logging.DEBUG,
+            force=True,
+        )
+    else:
+        raise Exception(
+            "The output needs to be either an svg path (/path/to/file.svg) or a render trace (/path/to/folder)."
+        )
+
+    logging.captureWarnings(True)
+
+    if args.use_camera:
+        renderer.set_camera(*reader.get_camera())
+
+    try:
+        timeline = reader.get_timeline(args.timeline)
+        with renderer:
+            for frame in list(timeline):
+                frame.render()
+                renderer.save_frame(frame)
+
+        renderer.compile(output_path, padding=args.padding, scale=args.scale)
+        print("done")
+    except:
+        print("error. Check logs.txt in the output folder for details.")
+        logging.exception(traceback.format_exc())
+
+
+def get_matching_path(input_root, output_root, input_path):
+    relpath = os.path.relpath(input_path, input_root)
+    return os.path.join(output_root, relpath)
+
+
+def splitext(path):
+    folder, filename = os.path.split(path)
+    if "." in filename:
+        name, ext = filename.rsplit(".", maxsplit=1)
+        return os.path.join(folder, name), f".{ext}"
+    return path, ""
+
+
+def main():
     parser = argparse.ArgumentParser()
-
-    # export to tables
     parser.add_argument(
         "input",
         type=str,
@@ -32,6 +101,25 @@ if __name__ == "__main__":
         "output",
         type=str,
         help="Output file or folder. This can be a render trace (/path/to/trace/) or an SVG (/path/to/file.svg).",
+    )
+    parser.add_argument(
+        "--batch",
+        action="store_true",
+        help="""Recursively process all files in a folder to generate the target type. If used, any option suffix (.xfl, .svg) is stripped off the input and output arguments, and the results are treated as folders that should contain inputs and outputs. Example to process XFL files in /input/root/ and write the resulting SVG files to /output/root/: seq 24 | xargs -L1 -P24 python -m xflsvg /input/root.xfl /output/root.svg --batch --use-camera --par 24 --id""",
+    )
+    parser.add_argument(
+        "--par",
+        type=int,
+        required=False,
+        default=1,
+        help="The total number of sibling processes running the same task in parallel. This is for parallel execution with xargs.",
+    )
+    parser.add_argument(
+        "--id",
+        type=int,
+        required=False,
+        default=1,
+        help="The sibling index of this process (0 through par-1). This is for parallel execution with xargs.",
     )
     parser.add_argument(
         "--timeline",
@@ -60,123 +148,36 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    if not args.batch:
+        convert(args.input, args.output, args)
+        return
 
-    input_path = os.path.normpath(args.input)
-    if input_path.lower().endswith(".xfl"):
-        input_folder = os.path.dirname(input_path)
-        reader = XflReader(input_folder)
-    elif os.path.isdir(input_path):
-        reader = RenderTraceReader(input_path)
-    else:
-        raise Exception(
-            "The input needs to be either an xfl file (/path/to/file.xfl) or a render trace (/path/to/trace/)."
-        )
+    input_folder, source_type = splitext(args.input)
+    output_folder, target_type = splitext(args.output)
 
-    output_path = os.path.normpath(args.output)
-    if output_path.lower().endswith(".svg"):
-        renderer = SvgRenderer()
-    elif os.path.isdir(output_path) or not os.path.exists(output_path):
-        renderer = RenderTracer()
-    else:
-        raise Exception(
-            "The output needs to be either an svg path (/path/to/file.svg) or a render trace (/path/to/folder)."
-        )
+    source_type = source_type.lower()
+    target_type = target_type.lower()
 
-    if args.use_camera:
-        renderer.set_camera(*reader.get_camera())
+    assert source_type in (".xfl", ""), "Input arg must end in either .xfl or /"
+    assert target_type in (".svg", ""), "Output arg must end in either .svg or /"
 
-    timeline = reader.get_timeline(args.timeline)
-    with renderer:
-        for frame in list(timeline):
-            frame.render()
-            renderer.save_frame(frame)
+    for root, dirs, files in os.walk(input_folder):
+        for fn in files:
+            if not should_process(os.path.join(root, fn), args):
+                continue
 
-    renderer.compile(output_path, padding=args.padding, scale=args.scale)
+            if source_type == ".xfl":
+                name, extension = splitext(fn)
+                if extension.lower() == ".xfl":
+                    input_path = os.path.join(root, fn)
+                    output_path = get_matching_path(input_folder, output_folder, root)
+                    convert(input_path, f"{output_path}/{target_type}", args)
+            elif source_type == "":
+                if fn.lower() == "frames.json":
+                    input_path = root
+                    output_path = get_matching_path(input_folder, output_folder, root)
+                    convert(input_path, f"{output_path}/{target_type}", args)
 
-    #     parser = argparse.ArgumentParser()
-    #     parser.add_argument('--export', required=True, action='store_true')
-    #     parser.add_argument('input_folder', type=str)
-    #     parser.add_argument('output_folder', type=str)
 
-    #     args = parser.parse_known_args()[0]
-
-    #     for root, dirs, files in os.walk(input_folder):
-    #         relpath = os.path.relpath(root, input_folder)
-    #         target_folder = os.path.join(args.output_folder, relpath)
-    #         if not should_process(target_folder, id, par):
-    #             continue
-
-    #         if not os.path.exists(os.path.join(root, 'DOMDocument.xml')):
-    #             continue
-
-    #         has_shapes = os.path.exists(os.path.join(target_folder, 'shapes.json'))
-    #         has_frames = os.path.exists(os.path.join(target_folder, 'frames.json'))
-    #         has_labels = os.path.exists(os.path.join(target_folder, 'labels.json'))
-    #         if has_shapes and has_frames and has_labels:
-    #             print('-- skipping', root)
-    #             continue
-
-    #         print('-- exporting', root)
-    #         os.makedirs(target_folder, exist_ok=True)
-    #         logging.basicConfig(filename=os.path.join(target_folder, 'logs.txt'), level=logging.DEBUG, force=True)
-    #         logging.captureWarnings(True)
-
-    #         try:
-    #             export(root, target_folder)
-    #         except:
-    #             print('error on', root)
-    #             logging.exception(traceback.format_exc())
-
-    # elif args.render:
-    #     parser = argparse.ArgumentParser()
-    #     parser.add_argument("--render", required=True, action='store_true')
-    #     parser.add_argument('format', type=str)
-    #     parser.add_argument('input_folder', type=str)
-    #     parser.add_argument('--timeline', required=True, type=str)
-    #     parser.add_argument('output_folder', type=str)
-    #     parser.add_argument('--width', required=False, type=float)
-    #     parser.add_argument('--height', required=False, type=float)
-    #     parser.add_argument('--x', required=False, type=float)
-    #     parser.add_argument('--y', required=False, type=float)
-    #     parser.add_argument('--scale', required=False, type=float)
-
-    #     args = parser.parse_known_args()[0]
-
-    #     os.makedirs(args.output_folder, exist_ok=True)
-    #     input_folder = os.path.abspath(args.input_folder)
-    #     with open(os.path.join(input_folder, 'shapes.json'), 'r') as inp:
-    #         shapes = json.load(inp)
-    #     with open(os.path.join(input_folder, 'frames.json'), 'r') as inp:
-    #         frames = json.load(inp)
-    #     with open(os.path.join(input_folder, 'labels.json'), 'r') as inp:
-    #         labels = json.load(inp)
-
-    #     candidates = set()
-    #     candidates.add(args.timeline)
-
-    #     timelines = {}
-    #     for frame_id, label in labels.items():
-    #         if 'timeline' not in label:
-    #             continue
-    #         symbol = label['timeline']
-    #         if symbol in candidates:
-    #             timelines.setdefault(symbol, []).append((frame_id, label['frame']))
-
-    #     for sym, timeline in enumerate(timelines.values()):
-    #         output_folder = f'{args.output_folder}/{os.path.basename(input_folder)}/{sym}'
-    #         os.makedirs(output_folder, exist_ok=True)
-    #         for i, frame_id in enumerate(timeline):
-    #             frame_id = frame_id[0]
-    #             print('=== rendering frame', frame_id, '===')
-    #             frame = get_table_frame(shapes, frames, frame_id)
-    #             with SvgRenderer() as renderer:
-    #                 frame.render()
-    #             x = args.x or 0
-    #             y = args.y or 0
-    #             width = args.width or labels[frame_id].get('width', 550)
-    #             height = args.height or labels[frame_id].get('height', 400)
-    #             scale = args.scale or 1
-    #             svg = renderer.compile(width, height, x, y, scale)
-
-    #             with open(os.path.join(output_folder, '%04d.svg' % i), 'w') as outp:
-    #                 svg.write(outp, encoding='unicode')
+if __name__ == "__main__":
+    main()
