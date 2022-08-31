b8451f8b767720635df1eb986610a9bc8685e3d8
Synthbot Anon <synthbot.anon@gmail.com>
2022-08-31 14:03:03 -0700

    forgot to add gif & png renderers

---
+++ src/xflsvg/gifrenderer.py
@@ -0,0 +1,45 @@
+import os
+from xml.etree import ElementTree
+
+from gifski import Gifski
+from wand.image import Image
+from wand.color import Color
+
+from .svgrenderer import SvgRenderer
+
+
+class GifRenderer(SvgRenderer):
+    def __init__(self, background="#0000"):
+        super().__init__()
+        self.background = Color(background)
+
+    def compile(self, output_filename, framerate=1 / 24, *args, **kwargs):
+        result = []
+        width, height = map(round, self.get_frame_dimensions())
+        xml_frames = super().compile(*args, **kwargs)
+
+        g = None
+        timestamp = 0
+
+        for xml in xml_frames:
+            svg = ElementTree.tostring(xml.getroot(), encoding="utf-8")
+            image = Image(blob=svg, background=self.background)
+
+            if g == None:
+                g = Gifski(image.width, image.height)
+                g.set_file_output(output_filename)
+
+            rgba = image.make_blob("RGBA")
+            g.add_frame_rgba(rgba, timestamp)
+            timestamp += framerate
+
+        if g:
+            g.finish()
+
+
+def splitext(path):
+    folder, filename = os.path.split(path)
+    if "." in filename:
+        name, ext = filename.rsplit(".", maxsplit=1)
+        return os.path.join(folder, name), f".{ext}"
+    return path, ""
---
+++ src/xflsvg/pngrenderer.py
@@ -0,0 +1,42 @@
+import os
+from xml.etree import ElementTree
+
+from wand.image import Image
+from wand.color import Color
+
+from .svgrenderer import SvgRenderer
+
+
+class PngRenderer(SvgRenderer):
+    def __init__(self, background="#0000"):
+        super().__init__()
+        self.background = Color(background)
+
+    def compile(self, output_filename=None, suffix=True, *args, **kwargs):
+        result = []
+        print("compiling to svg", end="... ", flush=True)
+        xml_frames = super().compile(*args, **kwargs)
+        print("done")
+
+        for i, xml in enumerate(xml_frames):
+            print("compiling frame", i, "to png", end="... ", flush=True)
+            svg = ElementTree.tostring(xml.getroot(), encoding="utf-8")
+            png = Image(blob=svg).make_blob("png", background=self.background)
+            print("done")
+            result.append(png)
+
+            if output_filename:
+                name, ext = splitext(output_filename)
+                sfx = suffix and "%04d" % i or ""
+                with open(f"{name}{sfx}{ext}", "wb") as outp:
+                    outp.write(png)
+
+        return result
+
+
+def splitext(path):
+    folder, filename = os.path.split(path)
+    if "." in filename:
+        name, ext = filename.rsplit(".", maxsplit=1)
+        return os.path.join(folder, name), f".{ext}"
+    return path, ""
